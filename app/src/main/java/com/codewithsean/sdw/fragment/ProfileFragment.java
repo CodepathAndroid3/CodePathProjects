2517a903c4b1d765fecf695242589475d33ba02b
wilmer999 <73921336+wilmer999@users.noreply.github.com>
2022-05-05 20:31:22 -0400

    implemented Camera on profile page and edited the profile page layout

--- app/src/main/java/com/codewithsean/sdw/fragment/ProfileFragment.java
+++ app/src/main/java/com/codewithsean/sdw/fragment/ProfileFragment.java
@@ -1,15 +1,16 @@
 package com.codewithsean.sdw.fragment;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,41 +19,30 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.codepath.asynchttpclient.AsyncHttpClient;
-import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.codewithsean.sdw.LoginActivity;
 import com.codewithsean.sdw.R;
-import com.codewithsean.sdw.adapters.ShowsAdapter;
-import com.codewithsean.sdw.models.Shows;
+import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseUser;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.parse.SaveCallback;
 
 import java.io.File;
-import java.util.ArrayList;
-
-import okhttp3.Headers;
 
 
 public class ProfileFragment extends Fragment {
 
-//    private EditText etDescription;
-//    private Button btnTakePic;
-//    private ImageView ivPostImage;
-//    private Button btnSubmit;
-//    private Button btnLogout;
-//    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 123;
-//    private File photoFile;
-//    public String photoFileName = "photo.jpg";
-
-    //PROFILE PAGE AS OF RN ONLY HAS A USERIMAGE AND A USERNAME
+    private static final String prof = "fragment";
+    private EditText etDescription;
+    private Button btnSave;
+    private File photoFile;
     private ImageView pfpUserImage;
     private TextView pfpUserName;
     private Button btnLogout;
-
+    private Button btnImage;
+    public String photoFileName = "photo.jpg";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 55;
 
 
     public ProfileFragment() {
@@ -72,9 +62,12 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        etDescription = view.findViewById(R.id.etDescription);
         pfpUserImage = view.findViewById(R.id.pfpUserImage);
         pfpUserName = view.findViewById(R.id.pfpUserName);
         btnLogout = view.findViewById(R.id.btnLogout);
+        btnImage = view.findViewById(R.id.btnImage);
+        btnSave = view.findViewById(R.id.btnSave);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,21 +79,29 @@ public class ProfileFragment extends Fragment {
         });
 
 
+        btnImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+          public void onClick(View view) {
+                launchCamera();
+            }
+        });
 
-//        etDescription = view.findViewById(R.id.etDescription);
-//        btnTakePic = view.findViewById(R.id.btnTakePic);
-//        ivPostImage = view.findViewById(R.id.ivPostImage);
-//        btnSubmit = view.findViewById(R.id.btnSubmit);
-//        btnLogout = view.findViewById(R.id.btnLogout);
-
-//        btnTakePic.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                launchCamera();
-//            }
-//        });
-
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String description = etDescription.getText().toString();
+                if (description.isEmpty()) {
+                    Toast.makeText(getContext(), "Description cannot be emtpy", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(photoFile == null || pfpUserImage.getDrawable() == null) {
+                    Toast.makeText(getContext(), "There's no image", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
     }
+
 
     private void goLoginActivity() {
         Intent i = new Intent(getContext(), LoginActivity.class);
@@ -108,6 +109,29 @@ public class ProfileFragment extends Fragment {
         //finish();
     }
 
+    private void launchCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
 
+        // wrap File object into a content provider
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            //registerForActivityResult(intent, );
+        }
+    }
+
+    public File getPhotoFileUri(String fileName) {
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), prof);
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(prof, "failed to create directory");
+        }
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+    }
 
 }
